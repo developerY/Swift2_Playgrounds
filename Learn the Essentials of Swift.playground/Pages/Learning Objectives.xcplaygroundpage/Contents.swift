0e8a1a03687a3c2c36f93192c5fe9341602a18b3
Developer <developer@ylabz.com>
2015-09-30 20:46:07 -0700

    reset Swift fundamentals playground

---
+++ Learn the Essentials of Swift.playground/Pages/Learning Objectives.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+//: > **Important**: This is a preliminary document for an API or technology in development. Apple is supplying this information to help you plan for the adoption of the technologies and programming interfaces described herein for use on Apple-branded products. This information is subject to change, and software implemented according to this document should be tested with final operating system software and final documentation. Newer versions of this document may be provided with future betas of the API or technology.
+//: # Learn the Essentials of Swift
+//:
+//: Your first lesson is presented in the form of a guided Swift _playground_, a type of file that lets you change and interact with the code directly in Xcode and see the result immediately. Playgrounds are great for learning and experimenting, and this one helps get you up to speed on fundamental Swift concepts.
+//:
+//: ## Learning Objectives
+//:
+//: At the end of the lesson, you’ll be able to:
+//:
+//: * Differentiate between a constant and a variable
+//:
+//: * Know when to use implicit and when to use explicit type declarations
+//:
+//: * Understand the advantage of using optionals and optional binding
+//:
+//: * Differentiate between optionals and implicitly unwrapped optionals
+//:
+//: * Understand the purpose of conditional statements and loops
+//:
+//: * Use `switch` statements for conditional branching beyond a binary condition
+//:
+//: * Use `where` clauses to impose additional constraints in conditional statements
+//:
+//: * Differentiate between functions, methods, and initializers
+//:
+//: * Differentiate between classes, structures, and enumerations
+//:
+//: * Understand syntax for (and basic concepts behind) inheritance and protocol conformance
+//:
+//: * Determine implicit types and find additional information using Xcode’s quick help shortcut (Option-click)
+//:
+//: * Import and use UIKit
+//:
+//: See [License](License) for this sample's licensing information.
+//: 
+//: [Next](@next)
